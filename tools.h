7421bf341e2f3b186fbceb083438798ca94b399d
asouiki <souikiamine@gmail.com>
2019-07-20 19:02:07 +0200

    REFACTO delete unused tools header and cpp files

--- tools.h
+++
@@ -1,7 +0,0 @@
-#include <SDL2/SDL.h>
-
-void sdl_loop();
-
-void draw_scaled_pixel(SDL_Renderer *, int, int, int);
-
-void draw_sprite(unsigned char *, unsigned char, unsigned char, unsigned char);
